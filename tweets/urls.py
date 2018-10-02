05355863ee9001ab0cbd5d580103062f7b2f9281
nigel <33957570+hiNigel@users.noreply.github.com>
2018-10-02 09:46:34 +0530

    2 oct
    added tweet app to root, redirect, and form in tweet list

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,6 +1,7 @@
 
 from django.conf.urls import url
 
+from django.views.generic.base import RedirectView
 from .views import (
 TweetCreateView, 
 TweetDetailView, 
@@ -11,7 +12,8 @@ TweetDeleteView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^$', RedirectView.as_view(url="/")),
+    url(r'^search/$', TweetListView.as_view(), name='list'),
     url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
